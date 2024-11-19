b2c9aa219f4a76419126fca83e8718bb75816e0b
byeolee1221 <zicsso1221@gmail.com>
2024-11-20 01:31:27 +0900

    feat: 비밀번호 해싱, iron session 적용, 회원가입 로직 구현

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,7 +2,12 @@
 
 import { PASSWORD_ERROR_MESSAGE, PASSWORD_MIN_LENGTH } from "@/lib/constants";
 import { PASSWORD_REGEX } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUsername = (username: string) => {
   return !username.includes("potato");
@@ -16,6 +21,32 @@ const checkPasswords = ({
   confirmPassword: string;
 }) => password === confirmPassword;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -25,9 +56,10 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .transform((username) => `🍕 ${username} 🍕`)
-      .refine(checkUsername, "No potatoes allowed"),
-    email: z.string().email().toLowerCase(),
+      // .transform((username) => `🍕 ${username} 🍕`)
+      .refine(checkUsername, "No potatoes allowed")
+      .refine(checkUniqueUsername, "이미 존재하는 사용자입니다."),
+    email: z.string().email().toLowerCase().refine(checkUniqueEmail, "이미 존재하는 이메일입니다."),
     password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_ERROR_MESSAGE),
     confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
   })
@@ -44,10 +76,31 @@ export const createAccount = async (prevState: unknown, formData: FormData) => {
     confirmPassword: formData.get("confirmPassword"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword
+      },
+      select: {
+        id: true
+      }
+    });
+
+    const cookie = await getIronSession(await cookies(), {
+      cookieName: "delicious-carrot",
+      password: process.env.COOKIE_PASSWORD!
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+
+    redirect("/profile");
   }
 };
---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+const profilePage = () => {
+  return (
+    <div>어서오세요!</div>
+  )
+}
+
+export default profilePage;
